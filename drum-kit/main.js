3d345ad017bb14fcbbff574f725dcd0923f33855
Ilya758 <subscriber6436@gmail.com>
2021-08-21 18:00:28 +0300

    feat: implemented interactive playing sounds

--- drum-kit/main.js
+++ drum-kit/main.js
@@ -1,4 +1,27 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM was successfully loaded');
+
+    function playSound(audio) {
+        const item = audio.parentElement.parentElement;
+        item.classList.add('item_state_active');
+        audio.play();
+        audio.currentTime = 0.005;
+        setTimeout(() => item.classList.remove('item_state_active'), 200);
+    }
+
+    document.addEventListener('keydown', (event) => {
+        const audio = document.querySelector(`audio[data-key=${event.code}]`);
+        if (audio) {
+            playSound(audio);
+        }
+    });
+
+    document.addEventListener('click', (event) => {
+        let pressedKey = event.target.parentElement.children[0].children[0].getAttribute('data-key') || event.target.children[0].children[0].getAttribute('data-key');
+        if (pressedKey) {
+            const audio = document.querySelector(`audio[data-key=${pressedKey}]`);
+            playSound(audio);
+        }
+    })
+
 });
