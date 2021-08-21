49521ebd83f10d40a0a4c7cd33505105b639b75d
Ilya758 <subscriber6436@gmail.com>
2021-08-21 23:30:23 +0300

    feat: implemented dark theme-toggler into page

--- drum-kit/main.js
+++ drum-kit/main.js
@@ -16,7 +16,10 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    document.addEventListener('click', (event) => {
+    const keysList = document.querySelector('.keys__list')
+
+    keysList.addEventListener('click', (event) => {
+
         let pressedKey = event.target.parentElement.children[0].children[0].getAttribute('data-key') || event.target.children[0].children[0].getAttribute('data-key');
         if (pressedKey) {
             const audio = document.querySelector(`audio[data-key=${pressedKey}]`);
@@ -24,4 +27,31 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    const slider = document.querySelector('.header__slider');
+
+    slider.addEventListener('click', () => {
+        const thumb = document.querySelector('.header__thumb');
+        const mSlider = slider.getBoundingClientRect();
+        const mThumb = thumb.getBoundingClientRect();
+        const item = document.querySelectorAll('.keys__item');
+        item.forEach((i) => i.classList.toggle('item_theme_dark'));
+        thumb.classList.toggle('header__thumb_state_active');
+        slider.classList.toggle('header__slider_state_active');
+        document.querySelector('.wrapper').classList.toggle('wrapper_theme_dark');
+        document.querySelector('.header__tooltip').classList.toggle('tooltip_theme_dark');
+        if (+getComputedStyle(thumb).left.replace('px', '') > 1) {
+            thumb.classList.add('header__thumb_state_reverse-move');
+            thumb.style.left = '1px';
+            setTimeout(() => {
+                thumb.classList.remove('header__thumb_state_reverse-move');
+            }, 510);
+        }
+        else {
+            thumb.classList.add('header__thumb_state_move');
+            thumb.style.left = mSlider.width - mThumb.width - slider.clientLeft * 2 - 1 + 'px';
+            setTimeout(() => {
+                thumb.classList.remove('header__thumb_state_move');
+            }, 510)
+        }
+    })
 });
